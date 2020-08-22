eaeb1f20bed036b2e3b9c377a5212cc681339ecb
Glacier-Programs <51843802+Glacier-Programs@users.noreply.github.com>
2020-08-21 21:32:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ jumping = False
 canjump = True
 jump_max = 100
 jump_height = 0
+airTime = 0
+maxAirTime = 5
 touchingPlatform = False
 
 #lantern
@@ -30,6 +32,14 @@ allSprites.add(lantern)
 
 platforms = pg.sprite.Group()
 enemies = pg.sprite.Group()
+
+#ground for demo
+ground1 = classes.Ground(win,0,[0,350],width=200)
+allSprites.add(ground1)
+platforms.add(ground1)
+ground2 = classes.Ground(win,0,[300,350],width=200)
+allSprites.add(ground2)
+platforms.add(ground2)
 
 #main
 done = False
@@ -44,7 +54,7 @@ while not done:
     #key presses
     pressed = pg.key.get_pressed()
     x,y = 0,0
-    if pressed[pg.K_SPACE] and not jumping and canjump:
+    if pressed[pg.K_SPACE] and not jumping and touchingPlatform:
         jumping = True
     if pressed[pg.K_a]:
         x -= 10
@@ -59,12 +69,13 @@ while not done:
             y -= 10
             jump_height += 10
     #checks if it should fall
-    if not pg.sprite.spritecollideany(player,platforms) and not jumping:
-        y += 10
-        #canjump = False
+    if not pg.sprite.spritecollideany(player,platforms):
+        touchingPlatform = False
     else:
         touchingPlatform = True
         canjump = True
+    if not touchingPlatform and not jumping:
+        y += 10
     #move stuffs
     player.move([x,y])
     lantern.move()
@@ -72,6 +83,7 @@ while not done:
     #draw stuffs
     for entity in allSprites:
         win.blit(entity.surf2,entity.rect)
+        print(entity)
     
     #update screen
     pg.display.flip()
